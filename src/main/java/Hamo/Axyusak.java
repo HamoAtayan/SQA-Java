5cc72cf8fc75a0e597af21b15d8ddc1da69e0d8c
Hmayak Atayan <hmayak.atayan@gmail.com>
2019-09-25 20:51:46 +0400

    N 2  add axyusak

---
+++ src/main/java/Hamo/Axyusak.java
@@ -0,0 +1,16 @@
+package Hamo;
+
+/**
+ * Created by: Hmayak on Sep, 2019
+ */
+public class Axyusak {
+
+    public static void main(String[] args) {
+        printAxyusak(3);
+    }
+    public static void printAxyusak(int a){
+        for (int i = 1; i <= 9; i++) {
+            System.out.println(a + " * "+  i + " = " + a * i);
+        }
+    }
+}
