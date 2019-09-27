d62849620e29226682f0d43cdafccfce1a8f7a38
Hmayak Atayan <hmayak.atayan@gmail.com>
2019-09-27 14:16:00 +0400

    Refactor code

---
+++ src/main/java/Hamo/lessonone/CityDistance.java
@@ -0,0 +1,15 @@
+package Hamo.lessonone;
+
+/**
+ * Created by: Hmayak on Sep, 2019
+ *
+ 8. Գրել ծրագիր, որը կառաջարկի մուտքագրել 2 քաղաքների կոորդինատները (latitude, longitude, դրանք double տիպի էն) եւ կհաշվի;
+ 1) 2 քաղաքների միջեւ հեռավորությունը
+ 2) դրանց միջեւ ընկած միջանկյալ կետը
+ */
+public class CityDistance {
+    public static void main(String[] args) {
+
+    }
+
+}
