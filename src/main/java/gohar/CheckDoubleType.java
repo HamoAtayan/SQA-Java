e692972b71cd835eb7f58cb742eab5dc9a5a5eac
Gohar Farmanyan <gohar.farmanyan@webbfontaine.com>
2019-09-27 17:52:43 +0400

    Arajin 3 xndir

---
+++ src/main/java/gohar/CheckDoubleType.java
@@ -0,0 +1,14 @@
+package main.java.gohar;
+
+import java.util.Scanner;
+
+/**
+ * Created by Gohar Farmanyan on September
+ */
+public class CheckDoubleType {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println(scanner.hasNextDouble());
+    }
+}
