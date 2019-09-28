05f3941f3d0baa29f98a28b2bd38468595abf480
christina.mnoyan <christina.mnoyan@webbfontaine.com>
2019-09-29 01:10:00 +0400

    Added 6th exercise and SumOfPrimeNumbers

---
+++ src/main/java/Christina/HomeWork_1/NextPrimeNumber.java
@@ -0,0 +1,42 @@
+package Christina.HomeWork_1;
+
+import java.util.Scanner;
+
+/**
+ * Created by Christina Mnoyan on Sep, 2019
+ */
+public class NextPrimeNumber {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any number:");
+        int input;
+        while ((input = sc.nextInt()) != 0) {
+            giveNextPrime(input);
+        }
+        if (input == 0) {
+            System.out.println("Done!");
+        }
+    }
+
+    public static boolean isPrime(int i) {
+        for (int j = 2; j <= i / 2; j++) {
+            if (i % j == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void giveNextPrime(int input) {
+        for (int i = 1; i < input; i++) {
+            boolean checkPrime = !isPrime(input);
+            if (checkPrime) {
+                input++;
+            }
+        }
+        System.out.println("The next prime number is " + input);
+    }
+}
+
+
---
+++ src/main/java/Christina/HomeWork_1/SumOfPrimeNumbers.java
@@ -0,0 +1,25 @@
+package Christina.HomeWork_1;
+
+/**
+ * Created by Christina Mnoyan on Sep, 2019
+ */
+public class SumOfPrimeNumbers {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i <= 100; i++) {
+            boolean isPrime = true;
+            for (int j = 2; j <=Math.sqrt(i); j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime) {
+                sum += i;
+            }
+        }
+        System.out.println(sum-1);
+    }
+}
+
+
