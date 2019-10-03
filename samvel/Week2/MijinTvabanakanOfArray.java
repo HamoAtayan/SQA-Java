5c09438f3f978ec01b63c5735f81eb510f3db0a1
samvelmkrtchyan93 <samvel.mkrtchyan.1993@gmail.com>
2019-10-03 15:39:52 +0400

    Week 2 ex. 1-5

---
+++ samvel/Week2/MijinTvabanakanOfArray.java
@@ -0,0 +1,14 @@
+package Week2;
+
+public class MijinTvabanakanOfArray {
+    public static void main (String[] args){
+        double[] arrayOfDoubles = new double[10];
+        double sum = 0;
+        for (int i=0; i<arrayOfDoubles.length; i++) {
+            arrayOfDoubles[i] = Math.random()*10;
+            System.out.println("arrayOfDoubles{" + i + "] = " + arrayOfDoubles[i]);
+            sum += arrayOfDoubles[i];
+        }
+        System.out.println("Mijin Tvabanakany = " + sum/arrayOfDoubles.length);
+    }
+}
