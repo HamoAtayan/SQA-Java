99e5d579c218f39b63be32a17c5473c8efe86318
samvelmkrtchyan93 <samvel.mkrtchyan.1993@gmail.com>
2019-10-04 14:39:16 +0400

    Ex.7

---
+++ samvel/Week2/DoesContainInput.java
@@ -0,0 +1,25 @@
+package Week2;
+
+import java.util.Scanner;
+
+public class DoesContainInput {
+    public static void main(String[] args) {
+        int[] randomArray = new int[10];
+        for (int i=0; i<randomArray.length; i++) {
+            randomArray[i] = (int)(Math.random()*10);
+            System.out.print(randomArray[i] + " ");
+        }
+        Scanner input = new Scanner(System.in);
+        int userInput = input.nextInt();
+
+        for (int i=0; i<randomArray.length; i++) {
+            if (randomArray[i] == userInput) {
+                System.out.println("Your array contains your number: and that number is - " + userInput);
+                break;
+            }
+            if (i == (randomArray.length - 1) && randomArray[i] != userInput) {
+                System.out.print("There are no matches with your number! ");
+            }
+        }
+    }
+}
