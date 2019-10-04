4bd22a8d4ce34308ac5b119681f87909b9984fb8
Hmayak Atayan <hmayak.atayan@gmail.com>
2019-10-04 17:57:05 +0400

    add medium number

---
+++ hamo/src/week2/MediumNumber.java
@@ -0,0 +1,17 @@
+package week2;
+
+/**
+ * Created by: Hmayak on Oct, 2019
+ */
+public class MediumNumber {
+    double sum = 0;
+    public static void main(String[] args) {
+
+    }
+    public double mediumSum(double [] arr){
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        return sum/arr.length;
+    }
+}
