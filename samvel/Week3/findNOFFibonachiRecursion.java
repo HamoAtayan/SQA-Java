cf0e0615f8c017f3e0294556f221c73254e3e790
samvelmkrtchyan93 <samvel.mkrtchyan.1993@gmail.com>
2019-10-17 15:38:22 +0400

    Exercise 8 - Fibonachi with recursion

---
+++ samvel/Week3/findNOFFibonachiRecursion.java
@@ -0,0 +1,15 @@
+package Week3;
+
+public class findNOFFibonachiRecursion {
+    //Fibonacci Series using Recursion
+
+    public static int findFibN (int n) {
+        if (n <= 1)
+            return n;
+        return (findFibN(n-1) + findFibN(n-2));
+    }
+    public static void main(String[] args) {
+        int n = 10;
+        System.out.println(findFibN(n));
+    }
+}
