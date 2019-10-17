8f52cd3e7209d5ebbdb043420ea43bfe0d05cb01
samvelmkrtchyan93 <samvel.mkrtchyan.1993@gmail.com>
2019-10-17 14:37:47 +0400

    Exercise 9 - Updated missing methods

--- samvel/Week3/Person.java
+++ samvel/Week3/Person.java
@@ -9,16 +9,16 @@ public class Person {
 
     //Set Person details
 
-    public void setPersonName(String personName) {
-        name = personName;
+    public void setPersonName(String name) {
+        this.name = name;
     }
 
-    public void setPersonAge(Integer personAge) {
-        age = personAge;
+    public void setPersonAge(Integer age) {
+        this.age = age;
     }
 
-    public void setPersonWorkplace(String personWorkplace) {
-        workplace = personWorkplace;
+    public void setPersonWorkplace(String workplace) {
+        this.workplace = workplace;
     }
 
     //Function to check if person works in given workplace
@@ -64,6 +64,105 @@ public class Person {
         return youngestName;
     }
 
+    //Find most repeating age
+
+    public static int mostRepeatingAge(Person[] arr) {
+        int counter = 1, tempCounter;
+        int repeatingAge = arr[0].age;
+        int temp;
+        for (int i=0; i<(arr.length-1); i++) {
+            temp = arr[i].age;
+            tempCounter = 0;
+            for (int j = 0; j < arr.length; j++) {
+                if (temp == arr[j].age) {
+                    tempCounter++;
+                }
+                if (tempCounter > counter) {
+                    repeatingAge = temp;
+                    counter = tempCounter;
+                }
+            }
+        }
+        System.out.println("Most repeating age in your Persons array is : " + repeatingAge);
+        return repeatingAge;
+    }
+
+    //Sort Persons by age
+
+    public static void sortByAge (Person[] arr) {
+        for (int i = 0; i<arr.length; i++) {
+            int minAge = arr[i].age;
+            int minId = i;
+            Person youngestPerson = arr[i];
+            for (int j = i+1; j<arr.length; j++) {
+                if (arr[j].age < minAge) {
+                    youngestPerson = arr[j];
+                    minId = j;
+                }
+            }
+            //Swapping
+            Person temp = arr[i];
+            arr[i] = youngestPerson;
+            arr[minId] = temp;
+            System.out.println(arr[i].age);
+        }
+    }
+
+    //Sort persons by name
+
+    public static void sortByName (Person[] arr) {
+        Person temp;
+        for (int i = 0; i<arr.length; i++) {
+            for (int j = i + 1; j<arr.length; j++) {
+                if (arr[i].name.compareTo(arr[j].name) > 0) {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+            System.out.println(arr[i].name + " ");
+        }
+    }
+
+    //Sort by workplace
+
+    public static void sortByWorkplace (Person[] arr) {
+        Person temp;
+        for (int i = 0; i<arr.length; i++) {
+            for (int j = i + 1; j<arr.length; j++) {
+                if (arr[i].workplace.compareTo(arr[j].workplace) > 0) {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+            System.out.println(arr[i].workplace + " ");
+        }
+    }
+
+    //Find most repeating name
+
+    public static String mostRepeatingAName(Person[] arr) {
+        int counter = 1, tempCounter;
+        String repeatingName = arr[0].name;
+        String temp;
+        for (int i=0; i<(arr.length-1); i++) {
+            temp = arr[i].name;
+            tempCounter = 0;
+            for (int j = 0; j < arr.length; j++) {
+                if (temp.equals(arr[j].name)) {
+                    tempCounter++;
+                }
+                if (tempCounter > counter) {
+                    repeatingName = temp;
+                    counter = tempCounter;
+                }
+            }
+        }
+        System.out.println("Most repeating age in your Persons array is : " + repeatingName);
+        return repeatingName;
+    }
+
     public static void main(String[] args) {
 
         Person[] listOfPersons = new Person[3];
@@ -90,7 +189,7 @@ public class Person {
         System.out.println("Please insert a workplace you want to check for a match: ");
         String userWorkplace = userInput.next();
 
-        //Check if there are any matches
+        //Check if there are any matches in workplace
 
         for (int i = 0; i<listOfPersons.length; i++) {
             listOfPersons[i].doesWorkHere(userWorkplace);
@@ -104,6 +203,25 @@ public class Person {
 
         youngestPerson(listOfPersons);
 
+        //Call function to find most repeating age in array of Persons
+
+        mostRepeatingAge(listOfPersons);
+
+        //Sort persons by age
+
+        sortByAge(listOfPersons);
+
+        //Sort by name
+
+        sortByName(listOfPersons);
+
+        //Sort by workplace
+
+        sortByWorkplace(listOfPersons);
+
+        //Find most repeating name
+
+        mostRepeatingAName(listOfPersons);
 
     }
 }
