96a228aa4259c2ad09dd4a7ba274205c8aff65de
Hmayak Atayan <hmayak.atayan@gmail.com>
2019-10-29 14:49:35 +0400

    add person sorting methods

--- hamo/src/week3/person/Main.java
+++ hamo/src/week3/person/Main.java
@@ -1,5 +1,7 @@
 package week3.person;
 
+import java.sql.SQLOutput;
+
 /**
  * Created by: Hmayak on Oct, 2019
  */
@@ -15,5 +17,32 @@ public class Main {
         System.out.println(PersonsAPI.getOldestPerson(persons));
         System.out.println("--------------------");
         System.out.println(PersonsAPI.getYoungestPerson(persons));
-    }
-}
+        System.out.println("-----------------");
+        System.out.println(PersonsAPI.getMostRepeatedAge(persons));
+        System.out.println("-----------------");
+        System.out.println(PersonsAPI.getDuplicatePerson(persons));
+
+        for (int i = 0; i < persons.length ; i++) {
+            System.out.println(PersonsAPI.sortByAge(persons)[i].age);
+            System.out.print(PersonsAPI.sortByAge(persons)[i].name);
+            System.out.print(PersonsAPI.sortByAge(persons)[i].company);
+            System.out.println();
+        }
+
+        for (int i = 0; i < persons.length ; i++) {
+            System.out.println(PersonsAPI.sortByName(persons)[i].age);
+            System.out.print(PersonsAPI.sortByName(persons)[i].name);
+            System.out.print(PersonsAPI.sortByName(persons)[i].company);
+            System.out.println();
+        }
+
+        for (int i = 0; i < persons.length ; i++) {
+            System.out.println(PersonsAPI.sortByCompany(persons)[i].age);
+            System.out.print(PersonsAPI.sortByCompany(persons)[i].name);
+            System.out.print(PersonsAPI.sortByCompany(persons)[i].company);
+        }
+
+            }
+        }
+
+
